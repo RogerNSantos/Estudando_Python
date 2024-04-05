bd657f905b902366a20854636d8d42b3c059a85b
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2024-04-05 17:57:40 -0300

    Add files via upload

--- Curso_Em_Video/Python03_Mundo02/Condicoes_IF_ELSE/Desafios/Desafio044_Gerenciado_De_Pagamentos.py
+++ Curso_Em_Video/Python03_Mundo02/Condicoes_IF_ELSE/Desafios/Desafio044_Gerenciado_De_Pagamentos.py
@@ -2,5 +2,37 @@
  CONDIÇÃO DE PAGAMENTO:
   - Á vista DINHEIRO/CHEQUE: 10% de desconto
   - Á vista no cartão:5% de desconto
-  - Em até 2x NO CARTÃO: preço normal
-  - 3x OU MAIS no cartão: 20% de juros """
+  - Em até 2x no CARTÃO DE CRÉDITO: preço normal
+  - 3x OU MAIS no CARTÃO DE CRÉDITO: 20% de juros """
+
+print("\033[4;30;43m{:=^40} \033[m".format(" Lojas Python"))
+
+preco = float(input("\nValor das compras R$ "))
+print("""\033[4;30;44m ====== OPÇÕES DE PAGAMENTOS ======\033[m
+[ 1 ] -> à vista dinheiro/pix.
+[ 2 ] -> à vista débito.
+[ 3 ] -> 2x no crtão crédito.
+[ 4 ] -> 3x ou mais no cartão de crédito.""")
+opcao = int(input("\033[4;30;44m======Qual a forma de pagamento? ====== \033[m"))
+
+if opcao == 1:
+    total = preco - (preco * 10 / 100)
+    print("Sua compra de R$ {} com 10% de desconto vai custar {}".format(preco, total))
+elif opcao == 2:
+    total = preco - (preco * 5 / 100)
+    print("Sua compra de R$ {} com 5% de desconto vai custar {}".format(preco, total))
+elif opcao == 3:
+    total = preco
+    parcela = total / 2
+    print("Sua compra de R$ {} séra parcelada em 2x de R$ {:.2f} sem juros.".format(preco, parcela))
+elif opcao == 4:
+    total = preco + (preco * 20 / 100)
+    totalParcelas = int(input("Quantas parcelas? "))
+    parcela = total / totalParcelas
+    print("Sua compra de R$ {} será parcela em {}x de R$ {:.2f} com 20% de juros.".format(preco, totalParcelas, parcela))
+else:
+    total = preco
+    print("\n\033[4;30;41m Opção de pagamento invalida. Tente novamente.\033[m")
+#print("Sua compra de {:.2f} vai custar R$ {} no final.".format(preco, total))
+
+
--- Curso_Em_Video/Python03_Mundo02/Condicoes_IF_ELSE/Desafios/Desafio045_Gamer_Pedra_Papel_Tesoura.py
+++ Curso_Em_Video/Python03_Mundo02/Condicoes_IF_ELSE/Desafios/Desafio045_Gamer_Pedra_Papel_Tesoura.py
@@ -1,1 +1,53 @@
 """ Crie um programa que faça o computador jogar JOKEROÔ com você. """
+
+print("\n\033[4;34;40m{:*^40}\033[m".format(" JOGO - JOKEMPÔ "))
+
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0,2)
+print("\n\033[1;36;40m{:^40}".format('''->-> Suas opções de jogadas <-<-\033[m
+[ 0 ] - PEDRA
+[ 1 ] - PAPEL
+[ 2 ] - TESOURA'''))
+jogador = int(input('\nQual sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!!!')
+sleep(1)
+print('->' * 12)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('<-' * 12)
+
+if computador == 0:
+    if jogador == 0:
+        print('EMPATE!')
+    elif jogador == 1:
+        print('VICÊ VENCEU!')
+    elif jogador == 2:
+        print('COMPUTADOR VENCEU!')
+    else:
+        print('JOGADA INVÁLIDA!!!')
+
+elif computador == 1:
+    if jogador == 0:
+        print('COMPUTADOR VENCEU!')
+    elif jogador == 1:
+        print('EMPATE!')
+    elif jogador == 2:
+        print('VOCÊ VANCEU!')
+    else:
+        print('JOGADA INVÁLIDA!!!')
+
+elif computador == 2:
+    if jogador == 0:
+        print('VOCÊ VENCEU!')
+    elif jogador == 1:
+        print('COMPUTADOR VENCEU!')
+    elif jogador == 2:
+        print('EMPATE!')
+else:
+    print('JOGADA INVÁLIDA!!!')
