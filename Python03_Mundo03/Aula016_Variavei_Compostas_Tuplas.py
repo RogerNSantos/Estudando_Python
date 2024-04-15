ef88c698f60b46f187fb8b8b9fe5d9b72d462888
Rogério Neves dos Santos <103578702+RogerNSantos@users.noreply.github.com>
2024-04-15 17:14:21 -0300

    Add files via upload

---
+++ Python03_Mundo03/Aula016_Variavei_Compostas_Tuplas.py
@@ -0,0 +1,7 @@
+lanche = 'Hamburguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche [1])
+print(lanche[-2])
+print(lanche[1:3]) # Desconsidera o último elemento
+print(lanche[2:])
+print(lanche[:2])
+print(lanche[-3:])
